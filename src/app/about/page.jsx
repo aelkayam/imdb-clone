6a500c552338f7388cd7d4486076c88434845676
Elkayam Melesse <elka@duck.com>
2024-03-13 11:16:04 +0200

    added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to my movie database website! This is one of my small web
+        projects in my journey to become a full-stack developer.
+      </p>
+
+      <p>
+        This website (just like the official IMDb website) is designed to
+        provide you with a comprehensive database of movies from all around the
+        world, along with the latest news, reviews, and trailers. You can search
+        for movies by title, director, actor, genre, or release date, making it
+        easy to find the perfect movie for any occasion.
+      </p>
+
+      <p>
+        Thank you for visiting my website and I hope you enjoy your time
+        browsing through the movie database. If you have any feedback or
+        suggestions, please feel free to contact me. Happy browsing!
+      </p>
+    </div>
+  );
+}
