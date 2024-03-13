fd1b30eeae333e39492a32c9561dd2ea8c372d02
Elkayam Melesse <elka@duck.com>
2024-03-13 11:35:21 +0200

    added NavBar

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/Components/Header";
 import Providers from "./Providers";
+import NavBar from "@/Components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Providers>
           <Header />
+          <NavBar />
           {children}
         </Providers>
       </body>
