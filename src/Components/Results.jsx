a15914dd613ceb82777d1795708877ad9ebbb990
Elkayam Melesse <elka@duck.com>
2024-03-13 12:12:06 +0200

    fetch data from tmdb

---
+++ src/Components/Results.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Results({ results }) {
+  return (
+    <div>
+      {results.map((result) => (
+        <div key={result.id}>
+          <h2>{result.original_title}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
