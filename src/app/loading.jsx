b5288880cb3c562eaed37d15fa191de6a6e626c4
Elkayam Melesse <elka@duck.com>
2024-03-13 13:08:47 +0200

    added movie cards

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Loading() {
   return (
     <div className="flex justify-center mt-16">
-      <Image className="h-52" src="spinner.svg" alt="loading" />
+      <Image
+        className="h-52"
+        src="spinner.svg"
+        alt="loading"
+        width={50}
+        height={50}
+      />
     </div>
   );
 }
